c5a075a29ebdf3b7f24bd171eaf4141d5d55efd7
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2024-07-15 08:53:01 +0200

    Add valid smoke test

--- src/test/java/io/jenkins/plugins/httpclient5/SmokeTest.java
+++ src/test/java/io/jenkins/plugins/httpclient5/SmokeTest.java
@@ -1,3 +1,27 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2024 strangelookingnerd
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 package io.jenkins.plugins.httpclient5;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -6,15 +30,22 @@ import java.net.URI;
 import org.apache.hc.client5.http.fluent.Request;
 import org.apache.hc.client5.http.fluent.Response;
 import org.apache.hc.core5.http.HttpStatus;
-import org.junit.jupiter.api.Test;
+import org.junit.Rule;
+import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
-import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
+import org.jvnet.hudson.test.RealJenkinsRule;
 
-@WithJenkins
-class SmokeTest {
+public class SmokeTest {
+
+    @Rule
+    public RealJenkinsRule rr = new RealJenkinsRule();
 
     @Test
-    void smokeTest(JenkinsRule r) throws Exception {
+    public void smokeTest() throws Throwable {
+        rr.then(SmokeTest::_smokeTest);
+    }
+
+    private static void _smokeTest(JenkinsRule r) throws Exception {
         URI uri = r.getURL().toURI();
         Response response = Request.get(uri).execute();
         assertEquals(HttpStatus.SC_OK, response.returnResponse().getCode());
